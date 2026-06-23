0740e8f58aa069f786c8eb6d681b0485a4ee820f
NhiLy <nhi.ly@katalon.com>
2026-06-23 16:15:09 +0700

    change

--- Scripts/Verify Correct Alarm Message 01/Script1529986178839.groovy
+++ Scripts/Verify Correct Alarm Message 01/Script1529986178839.groovy
@@ -1,10 +1,22 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
-import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.util.internal.PathUtil
-
-import internal.GlobalVariable
+import com.kms.katalon.core.util.internal.PathUtil as PathUtil
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import org.openqa.selenium.Keys as Keys
 
 Mobile.comment('Story: Update text message')
 
@@ -18,6 +30,8 @@ Mobile.startApplication(appPath, false)
 Mobile.comment('And he navigates the application to Activity form')
 
 Mobile.tap(findTestObject('Application/android.widget.TextView - App'), 10)
+
+WebUI.delay(160000)
 
 Mobile.tap(findTestObject('Application/App/android.widget.TextView - Activity'), 10)
 
@@ -34,5 +48,4 @@ Mobile.comment('Then the correct dialog message should be displayed')
 Mobile.verifyEqual(message, 'Example of how you can use a custom Theme.Dialog theme to make an activity that looks like a customized dialog, here with an ugly frame.')
 
 Mobile.closeApplication()
-
 
---
+++ Test Suites/test align.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test align</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>64fa5a17-9346-4427-b06b-61e6c8fcb706</testSuiteGuid>
+   <testCaseLink>
+      <guid>497b64c4-666c-438d-86e7-2c736dfa7395</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify Correct Alarm Message 01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
